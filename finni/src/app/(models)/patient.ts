300430fc7f657de86687fdacccdb586787b7b023
Chris Park <cp3063@columbia.edu>
2023-11-18 15:50:34 -0800

    Creates patient model. Issues with mongoose.create() method timing out in /api/patients/route.ts

--- finni/src/app/(models)/patient.ts
+++ finni/src/app/(models)/patient.ts
@@ -1,15 +1,21 @@
-import mongoose, {Schema} from 'mongoose'
+import mongoose, { Schema } from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URI!)
-mongoose.Promise = global.Promise
+const MONGODB_URI = `mongodb+srv://chrispark:FinniHealth@finnicluster.xrymtmg.mongodb.net/`
+
+async function connect() {
+  const status = await mongoose.connect(MONGODB_URI);
+}
+mongoose.Promise = global.Promise;
 
 const patientSchema = new Schema({
-  firstName: {type: String, required: true},
+  firstName: { type: String, required: true },
   middleName: String,
-  lastName: {type: String, required: true},
-  status: {type: String, required: true},
-  address: {type: String, required: true},
-})
+  lastName: { type: String, required: true },
+  status: { type: String, required: true },
+  address: { type: String, required: true },
+});
 
-const Patient = mongoose.models.Patient ?? mongoose.model("Patient", patientSchema)
-export default Patient
+const Patient =
+  mongoose.models.Patient ?? mongoose.model("Patient", patientSchema);
+// const Patient = new mongoose.model('patient', patientSchema)
+export default Patient;
--- finni/src/app/api/patients/route.ts
+++ finni/src/app/api/patients/route.ts
@@ -1,12 +1,30 @@
-import {NextRequest, NextResponse} from 'next/server'
-import Patient from '~/app/(models)/patient'
+import { type NextRequest, NextResponse } from "next/server";
+import Patient from "~/app/(models)/patient";
+
+interface NewPatientInterface {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  status: string;
+  address: string;
+  // [additionalField: string]: string | number | undefined
+}
+
+type QueryBody = Record<string, string | number | undefined>;
 
 export async function POST(req: NextRequest): Promise<NextResponse<unknown>> {
   try {
-    const body: unknown = await req.json();
-    console.log(body)
-    return NextResponse.json({message: "success"})
+    const queryBody: QueryBody = {};
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const body: NewPatientInterface = await req.json();
+    for (const key in body) {
+      queryBody[key] = body[key];
+    }
+    console.log("QueryBody", queryBody);
+    const patient = await Patient.create(queryBody as QueryBody);
+    console.log("patient", patient)
+    return NextResponse.json({ message: "New patient successfully added." });
   } catch (error) {
-    return NextResponse.json({message: "Error", error })
+    return NextResponse.json({ message: "Error in New Patient API", error });
   }
 }
