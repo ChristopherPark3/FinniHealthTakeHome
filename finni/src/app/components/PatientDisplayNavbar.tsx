9d3d96124099d23608b601a074af97910fde98e6
Chris Park <cp3063@columbia.edu>
2023-11-25 21:50:26 -0800

    Sets up provider and client for react query

--- finni/src/app/components/PatientDisplayNavbar.tsx
+++ finni/src/app/components/PatientDisplayNavbar.tsx
@@ -3,24 +3,28 @@ import AddPatientButton from "./AddPatientButton";
 import PatientViewToggle from "./PatientViewToggle";
 import { POSTHelper } from "~/(hooks/helpers)/POSTHelper";
 import { PatientFilterContext } from "./PatientDisplayContainer";
-import { Input } from "@chakra-ui/react";
+import { Button, Input } from "@chakra-ui/react";
 
 export default function PatientDisplayNavbar() {
   // Get context for patient filter
-  const [filterParam, setFilterParam] = useContext(PatientFilterContext)
+  const [filterParam, setFilterParam] = useContext(PatientFilterContext);
 
   // handler for field equality search
   const handleSearch = (e: SyntheticEvent) => {
-    const searchParameter: string = e.target.value 
-    setFilterParam(searchParameter)
-  }
-  
+    const searchParameter: string = e.target.value;
+    setFilterParam(searchParameter);
+  };
 
   return (
-    <div id="Patient-Display" className="mt-[3rem] flex gap-4">
-      <Input onChange={handleSearch} borderColor="grey" placeholder="Search by name" />
+    <div id="Patient-Display" className="mt-[3rem] flex gap-4 ">
+      <Input
+        onChange={handleSearch}
+        borderColor="grey"
+        placeholder="Search by name"
+      />
       <PatientViewToggle />
       <AddPatientButton />
+      <Button>Add field</Button>
     </div>
   );
 }
--- finni/src/app/page.tsx
+++ finni/src/app/page.tsx
@@ -2,13 +2,18 @@
 
 import Header from "./components/Header";
 import PatientDisplayContainer from "./components/PatientDisplayContainer";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 export default function HomePage() {
 
   return (
+    <QueryClientProvider client={queryClient}>
     <div>
       <Header />
       <PatientDisplayContainer />
     </div>
+    </QueryClientProvider>
   );
 }
